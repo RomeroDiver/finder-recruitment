87ba7bd8d393138622ae4f5daf5b7af7b6491fb1
Roman Pylypets <roman.pylypets@hotmail.com>
2017-07-03 23:16:51 +0200

    Prepare project for tests

---
+++ task-3/src/components/Table/index.spec.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Table from './';
+import { StyledHeadTh } from './styled-components';
+
+describe('Table', () =>{
+    const cols = ['Animal', 'Colour', 'Price'];
+    const rows = (
+        <tr>
+            <td>Lion</td>
+            <td>Yellow</td>
+            <td>100</td>            
+        </tr>
+    );
+
+    it('renders welcome message', () => {
+        const component = shallow(<Table columns={cols}>{rows}</Table>);
+        const heads = component.find(StyledHeadTh);
+        expect(heads.length).toEqual(3);
+    });
+});
+
